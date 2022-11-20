a0c6d3737d96b9869d85bc705a14f56fc81ffb70
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-11-20 22:17:49 +0800

    Switch expect() to a match expr for err handling

--- 2/guessing_game/src/main.rs
+++ 2/guessing_game/src/main.rs
@@ -6,17 +6,25 @@ fn main() {
     println!("Guess the number hehe!");
 
     let secret_num = rand::thread_rng().gen_range(1..=100);
-    // println!("secret num: {secret_num}");
+    println!("secret num: {secret_num}");
 
     loop {
         println!("Kindly enter your guess here.");
 
         let mut guess = String::new();
 
-        io::stdin().read_line(&mut guess).expect("Failed to read");
-        println!("You guessed: {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read");
 
-        let guess: u32 = guess.trim().parse().expect("Kindly type a number.");
+        let guess: u32 = match guess
+            .trim()
+            .parse(){
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+
+        println!("You guessed: {}", guess);
         match guess.cmp(&secret_num) {
             Ordering::Less => println!("A tad bit too small."),
             Ordering::Greater => println!("Whoops! Too big."),
