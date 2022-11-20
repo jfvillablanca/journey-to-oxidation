64f07a3c110297e0ec208ccf32acb6b73efca66b
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-11-20 22:10:37 +0800

    Add game loop

--- 2/guessing_game/src/main.rs
+++ 2/guessing_game/src/main.rs
@@ -1,28 +1,29 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number hehe!");
 
-    let secret_num = rand::thread_rng()
-        .gen_range(1..=100);
+    let secret_num = rand::thread_rng().gen_range(1..=100);
+    // println!("secret num: {secret_num}");
 
-    println!("Kindly enter your guess here.");
+    loop {
+        println!("Kindly enter your guess here.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read");
+        io::stdin().read_line(&mut guess).expect("Failed to read");
+        println!("You guessed: {}", guess);
 
-    let guess: u32 = guess.trim().parse().expect("Kindly type a number.");
-
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_num){
-        Ordering::Less => println!("A tad bit too small."),
-        Ordering::Greater => println!("Whoops! Too big."),
-        Ordering::Equal => println!("Congratulations! You guessed it.")
+        let guess: u32 = guess.trim().parse().expect("Kindly type a number.");
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("A tad bit too small."),
+            Ordering::Greater => println!("Whoops! Too big."),
+            Ordering::Equal => {
+                println!("Congratulations! You guessed it.");
+                break;
+            }
+        }
     }
 }
