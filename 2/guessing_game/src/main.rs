1f2ca97c797c222df261a898f1aae567259b7393
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-11-20 21:58:49 +0800

    Add rand secret num func

--- 2/guessing_game/src/main.rs
+++ 2/guessing_game/src/main.rs
@@ -1,7 +1,13 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number hehe!");
+
+    let secret_num = rand::thread_rng()
+        .gen_range(1..=100);
+
     println!("Kindly enter your guess here.");
 
     let mut guess = String::new();
@@ -10,5 +16,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read");
 
+    let guess: u32 = guess.trim().parse().expect("Kindly type a number.");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_num){
+        Ordering::Less => println!("A tad bit too small."),
+        Ordering::Greater => println!("Whoops! Too big."),
+        Ordering::Equal => println!("Congratulations! You guessed it.")
+    }
 }
