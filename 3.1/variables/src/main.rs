6ad0fb3929343d3299e687fd8b7c2205204f344d
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-11-21 22:49:06 +0800

    Insert variables exercise

--- 3.1/variables/src/main.rs
+++ 3.1/variables/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("Value of x is: {x}");
+    x = 6;
+    println!("Value of x is: {x}");
+
+    let y = 5;
+    let y = y + 1;
+    {
+        let y = y * 2;
+        println!("The value of y in the inner scope is: {y}");
+    }
+    println!("The value of y is: {y}");
 }
