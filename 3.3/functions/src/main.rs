1a9f71c31e3c80f49419e8c70576e821731634c3
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-11-23 19:26:30 +0800

    Func with params

--- 3.3/functions/src/main.rs
+++ 3.3/functions/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     println!("Hello, world!");
-    new_func();
+    new_func(69);
 }
 
-fn new_func() {
-    println!("I am in another function lol");
+fn new_func(x: u8) {
+    println!("I like {x}");
 }
