33af03ef263f2e79d2b89fc905174b746efc50ca
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-11-28 20:44:27 +0800

    New concept, block scope expressions!

--- 3.3/functions/src/main.rs
+++ 3.3/functions/src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
     println!("Hello, world!");
-    new_func(69,'😎');
+    new_func(69, '😎');
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
 }
 
 fn new_func(x: u8, emoji: char) {
