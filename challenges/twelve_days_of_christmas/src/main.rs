6232ef074356f06b100139725fae0d885699f0bb
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-04 19:04:14 +0800

    Add the 12 if arms

---
+++ challenges/twelve_days_of_christmas/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    for day in 1..12 {
+        if day == 1 {
+            println!("On the first day of Christmas");
+        } else if day == 2 {
+            println!("On the second day of Christmas");
+        } else if day == 3 {
+            println!("On the third day of Christmas");
+        } else if day == 4 {
+            println!("On jhe fourth day of Christmas");
+        } else if day == 5 {
+            println!("On the fifth day of Christmas");
+        } else if day == 6 {
+            println!("On the sixth day of Christmas");
+        } else if day == 7 {
+            println!("On the seventh day of Christmas");
+        } else if day == 8 {
+            println!("On the eighth day of Christmas");
+        } else if day == 9 {
+            println!("On the ninth day of Christmas");
+        } else if day == 10 {
+            println!("On the tenth day of Christmas");
+        } else if day == 11 {
+            println!("On the eleventh day of Christmas");
+        } else if day == 12 {
+            println!("On the twelfth day of Christmas");
+        }
+        println!("my true love sent to me:");
+    }
+}
