b0d57ddd8bbff10d063e16a0af31313c0742f530
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-04 21:54:06 +0800

    Finish :)

--- challenges/twelve_days_of_christmas/src/main.rs
+++ challenges/twelve_days_of_christmas/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
-    for day in 1..12 {
+    let mut gifts: String = "".to_owned();
+    let mut gift; 
+    
+    for day in 1..13 {
         if day == 1 {
             println!("On the first day of Christmas");
         } else if day == 2 {
@@ -26,5 +29,37 @@ fn main() {
             println!("On the twelfth day of Christmas");
         }
         println!("my true love sent to me:");
+        if day == 1 {
+            println!("A partridge in a Pear Tree");
+            gift = format!("and a partridge in a Pear Tree");
+            gifts.insert_str(0, &gift);
+        } else {
+            if day == 12 {
+                gift = format!("{} Drummers Drumming\n", day);
+            } else if day == 11 {
+                gift = format!("{} Pipers Piping\n", day);
+            } else if day == 10 {
+                gift = format!("{} Lords a Leaping\n", day);
+            } else if day == 9 {
+                gift = format!("{} Ladies Dancing\n", day);
+            } else if day == 8 {
+                gift = format!("{} Maids a Milking\n", day);
+            } else if day == 7 {
+                gift = format!("{} Swans a Swimming\n", day);
+            } else if day == 6 {
+                gift = format!("{} Geese a Laying\n", day);
+            } else if day == 5 {
+                gift = format!("{} Golden Rings\n", day);
+            } else if day == 4 {
+                gift = format!("{} Calling Birds\n", day);
+            } else if day == 3 {
+                gift = format!("{} French Fries\n", day);
+            } else {
+                gift = format!("{} Turtle Doves\n", day);
+            }
+            gifts.insert_str(0, &gift);
+            println!("{gifts}");
+        }
+        println!("");
     }
 }
