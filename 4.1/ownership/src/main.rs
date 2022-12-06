5869310dd3d197a5106a5039a39ff6b0b1cfc4f0
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-06 20:48:20 +0800

    String clones! Deep copy

--- 4.1/ownership/src/main.rs
+++ 4.1/ownership/src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
-    let mut s = String::from("hello");
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
 
-    s.push_str(", world!"); // push_str() appends a literal to a String
-
-    println!("{}", s); // This will print `hello, world!`
+    println!("s1 = {}, s2 = {}", s1, s2);
 }
