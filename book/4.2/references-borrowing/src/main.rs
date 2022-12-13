f09d7144a9c4d4301701eba275bd587aee72dcda
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2022-12-13 18:08:28 +0800

    Multiple immutable references at the same time

--- book/4.2/references-borrowing/src/main.rs
+++ book/4.2/references-borrowing/src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
     let mut s = String::from("hello");
 
-    change(&mut s);
-}
+    let r1 = &s;                         // no problem
+    let r2 = &s;                         // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
 
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+    let r3 = &mut s;                     // no problem
+    println!("{}", r3);
 }
